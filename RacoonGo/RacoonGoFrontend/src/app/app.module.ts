16f5f08d3f2bba0037cec56505f6b050083f1891
Ibonek <al394759@uji.es>
2023-03-08 16:12:47 +0100

    sp01(Add component)committed login component

--- RacoonGo/RacoonGoFrontend/src/app/app.module.ts
+++ RacoonGo/RacoonGoFrontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { EventFormComponent } from './event-form/event-form.component';
 import { AccountComponent } from './user/account/account.component';
+import { LoginComponent } from './login/login.component';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,6 +32,7 @@ const analytics = getAnalytics(app);
     AppComponent,
     EventFormComponent,
     AccountComponent,
+    LoginComponent,
   ],
     imports: [
         BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule
