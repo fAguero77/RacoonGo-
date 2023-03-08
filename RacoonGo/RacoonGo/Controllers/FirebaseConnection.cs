566327057030253a1d7b669c5a6f5981a2422e35
pxe05 <79581464+pxe05@users.noreply.github.com>
2023-03-08 16:07:14 +0100

    sp01(connection of firebase and net created)

--- RacoonGo/RacoonGo/Controllers/FirebaseConnection.cs
+++ RacoonGo/RacoonGo/Controllers/FirebaseConnection.cs
@@ -1,7 +1,22 @@
-﻿namespace RacoonGo.Controllers;
+﻿using FireSharp;
+using FireSharp.Config;
 
+namespace RacoonGo.Controllers;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 public class FirebaseConnection
 {
-    IFirebaseConfig config=
+    IFirebaseClient cliente;
+    public FirebaseConnection()
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "hdYoKtTxDhfxoKF34JXlwXVSsclVI9c8uHu8vebZ",
+            BasePath = "https://racoongo-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+        
+        cliente=new FirebaseClient(config);
+    }
+    
 }
