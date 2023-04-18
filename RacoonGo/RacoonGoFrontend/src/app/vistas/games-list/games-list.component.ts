745728cbee9ea449be2eedcf714445487a585c95
pxe05 <79581464+pxe05@users.noreply.github.com>
2023-04-19 00:54:50 +0200

    sp03(RG-14): nuevo componente para listar juegos

---
+++ RacoonGo/RacoonGoFrontend/src/app/vistas/games-list/games-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games-list',
+  templateUrl: './games-list.component.html',
+  styleUrls: ['./games-list.component.css']
+})
+export class GamesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
